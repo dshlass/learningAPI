1de6106f8b7bcb22c24f464a25c8962088545832
Dale Shlass <dale@shlass.com>
2019-12-15 16:59:27 -0500

    redux added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import './App.css';
 import SearchFood from './components/SearchFood/SearchFood'
-import SelectedFood from './components/SelectedFood/SelectedFood'
+// import SelectedFood from './components/SelectedFood/SelectedFood'
 
 const App: React.FC = () => {
   return (
     <div className="App">
         <SearchFood />
-        <SelectedFood />
+        {/* <SelectedFood /> */}
     </div>
   );
 }
